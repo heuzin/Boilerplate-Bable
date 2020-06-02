61985647843c7b814e083420fb12f478d1c87326
heuzin <matheusqs.arq@gmail.com>
2020-06-02 12:15:27 -0400

    clean boilerplate

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import square, { add, name } from './utilities'
-import scream from './scream'
-
-console.log('index.js')
-console.log(add(30, 2))
-console.log(scream(name))
-console.log(square(20))
