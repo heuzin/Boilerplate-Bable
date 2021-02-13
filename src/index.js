c11d4959bcff81776909a6482b2086e6e9f648aa
heuzin <matheusqs.arq@gmail.com>
2021-02-12 21:48:41 -0300

    updating boilerplate

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// REST OPERATOR
 const calculateAverage = (thing, ...numbers) => {
     //return (numOne + numTwo) / 2
     let sum = 0
@@ -21,3 +22,80 @@ const printTeam = (teamName, coach, ...players) => {
 }
 
 printTeam('Liberty', 'Casey Penn', 'Marge', 'Aiden', 'Herbert', 'Sherry')
+
+
+// SPREAD OPERATOR
+const team = {
+    name : 'Liberdade',
+    coach: 'Heuzin',
+    players: ['Marcelo Moreno', 'Ricardo Goulard', 'Everton Ribeiro']
+}
+
+printTeam(team.name, team.coach, ...team.players)
+
+const cities = ['Barcelona', 'Cape Town', 'Bordeaux']
+const citiesCopy = [...cities, 'Santiago']
+
+console.log(cities)
+console.log(citiesCopy)
+
+let house = {
+    bedrooms: 2,
+    bathrooms: 1.5,
+    yearBuilt: 2017
+}
+
+let newHouse = {
+    ...house,
+    bedrooms: 5,
+    kitchen: 2
+}
+
+console.log(house)
+console.log(newHouse)
+
+// OBJECT SPREAD OPERATOR
+const person = {
+    name: 'Matheus',
+    age: '27'
+}
+
+const location = {
+    city: 'Bridgeport',
+    country: 'USA'
+}
+
+const overview = {
+    ...person,
+    ...location
+}
+
+console.log(overview)
+
+// DESTRUCTING
+const todo = {
+    id: 'fasqqgerghehrew',
+    text: 'Pay bills',
+    completed: false
+}
+
+const { text:todoText, completed, details = 'No details provided', ...others } = todo
+
+console.log(todoText)
+console.log(completed)
+console.log(details)
+console.log(others)
+
+
+// array
+const age = [65, 0, 13]
+const [firstAge, secondAge, , lastAge = 123] = age
+
+console.log(firstAge)
+console.log(lastAge)
+
+// function
+const printTodo =  ({ text, completed }) => {
+    console.log(`${text}: ${completed}`)
+}
+printTodo(todo)
